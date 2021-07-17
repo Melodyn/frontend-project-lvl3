207ddf68065c2c0aabc77033e727ea52f0c4c6c3
Melodyn <samelodyn@gmail.com>
2021-07-17 15:48:16 +0300

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,7 @@
-// const Hwp = require('html-webpack-plugin');
-//
-// module.exports = {
-//   plugins: [
-//     new Hwp({
-//       title: 'RSS Aggregator',
-//     }),
-//   ],
-// };
-
 import Hwp from 'html-webpack-plugin';
 
 export default {
   mode: process.env.NODE_ENV || 'development',
-  watch: true,
   plugins: [
     new Hwp({
       template: 'src/index.html',
